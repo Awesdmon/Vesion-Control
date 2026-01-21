ee7a7b0b323f5623d851766ea87f5b62d6c16ccb
Awesdmon <Gabrielpierre28@gmail.com>
2026-01-21 14:28:26 -0500

    next commit

---
+++ My project/Assets/BallStopTracker.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class BallStopTracker : MonoBehaviour
+{
+    public Transform targetTransform;   // The transform that will receive the position
+    public float stopThreshold = 0.05f;  // How slow is "stopped"
+
+    private Rigidbody rb;
+    private bool hasStopped = false;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        if (hasStopped)
+            return;
+        // Optional: only trigger on floor
+        if (!collision.gameObject.CompareTag("Floor"))
+            return;
+
+        if (rb.linearVelocity.magnitude < stopThreshold)
+        {
+            hasStopped = true;
+            targetTransform.position = transform.position;
+        }
+    }
+}
--- My project/Assets/Scripts/GameController.cs
+++ My project/Assets/Scripts/GameController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public Transform KylePosition;
+    Transform CameraPosition;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
